a6f0755f204e7b3bd8239c7868989313b79a593a
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-17 09:35:16 -0500

    Added a dictionary and values for Hercules and the attackers

---
+++ main.py
@@ -0,0 +1,24 @@
+hercules_stats = {
+    'health' : 100,
+    'attack power' : 20,
+    'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
+}
+
+goblin_stats = {
+    'health' : 25,
+    'attack power' : 7,
+    'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
+}
+
+brute_stats = {
+    'health' : 120,
+    'attack power' : 15,
+    'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
+}
+
+gnome_stats = {
+    'health' : 25,
+    'attack power' : 130,
+    'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
+}
+
