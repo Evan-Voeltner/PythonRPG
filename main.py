b390712dee6d1a78551a99ba1c23a5458daa3c70
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-17 09:52:37 -0500

    Added a Get_Random() function to return an item from a list, and set the attacker_attack variable to a random attack

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 hercules_stats = {
     'health' : 100,
     'attack power' : 20,
@@ -22,6 +24,15 @@ gnome_stats = {
     'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
 }
 
-def Attack(player, attacker):
-    player_health = player.get('health')
-    attacker_health = attacker.get('health')
+def Get_Random(given_list):
+    chosen_item = random.sample(given_list, k=1)
+    return(chosen_item.pop())
+
+def Attack(given_player, given_attacker):
+    player_health = given_player.get('health')
+    player_power = given_player.get('attack power')
+    player_attack = ''
+    attacker_health = given_attacker.get('health')
+    attacker_power = given_attacker.get('attack power')
+    attacker_attack = Get_Random(given_attacker.get('attack names'))
+    
