ec8fcf3d1e2c2d7a55a76ea5829443a8222f99ba
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-17 10:14:55 -0500

    Added a 'name' key to each dictionary and subsequent variables in the Attack() function. Started in the attack logic and gameplay between the player and attacker.

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import random
 
 hercules_stats = {
+    'name' : 'Hercules',
     'health' : 100,
     'attack power' : 20,
     'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
 }
 
 goblin_stats = {
+    'name' : 'Goblin',
     'health' : 25,
     'attack power' : 7,
     'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
 }
 
 brute_stats = {
+    'name' : 'Brute',
     'health' : 120,
     'attack power' : 15,
     'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
 }
 
 gnome_stats = {
+    'name' : 'Gnome',
     'health' : 25,
     'attack power' : 130,
     'attack names' : ['Slash', 'Stab', 'Shove', 'Kick', 'Punch']
@@ -29,10 +33,29 @@ def Get_Random(given_list):
     return(chosen_item.pop())
 
 def Attack(given_player, given_attacker):
+    player_name = given_player.get('name')
     player_health = given_player.get('health')
     player_power = given_player.get('attack power')
     player_attack = ''
+    attacker_name = given_attacker.get('name')
     attacker_health = given_attacker.get('health')
     attacker_power = given_attacker.get('attack power')
-    attacker_attack = Get_Random(given_attacker.get('attack names'))
-    
+    attacker_attack = ''
+
+    print(f'A {attacker_name} has appeard!')
+
+    while player_health > 0 and attacker_health > 0:
+        for name in given_player.get('attack names'):
+            print(name)
+        player_attack = input('Pick an attack!')
+
+        attacker_health = attacker_health =- player_power
+        print(f'{player_name} attacks {attacker_name} with {player_attack}!')
+        print(f'{attacker_name} has {str(attacker_health)}')
+
+        attacker_attack = Get_Random(given_attacker.get('attack names'))
+        print(f'{attacker_name} attacks {player_name} with {attacker_attack}!')
+        print(f'{player_name} has {str(player_health)}')
+
+
+Attack(hercules_stats, goblin_stats)
